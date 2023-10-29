8ee68dda0d55d422dd19187aef4293c9daf19b1d
Matheus Costa <matheuscostakrenn@gmail.com>
2023-10-28 22:39:45 -0300

    remove debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,5 @@
 import routes from '../../src/routes.mjs';
 
 window.addEventListener('load', function handleWindowLoad(event) {
-    // routes.navigateTo('open-file');
-
-    fetch('/tests/json/giant.json', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function handleFetchResponse(response) {
-        return response.json();
-    }).then(function handleFetchData(data) {
-        routes.navigateTo('view-file-content', {
-            fileName: 'giant.json',
-            fileData: data
-        });
-    }).catch(function handleFetchError(error) {
-        console.error(error);
-    });
-
-    // DEBUG
-    // routes.navigateTo('view-file-content', {
-    //     fileName: 'test.json',
-    //     fileData: '{"name": "John Doe", "age": 30, "cars": ["Ford", "BMW", "Fiat"]}'
-    // });
+    routes.navigateTo('open-file');
 });
